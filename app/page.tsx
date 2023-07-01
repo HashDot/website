f8543d94d2019c21499e9a4c6a971e549a92f161
Lars Hampe <hello@hashdot.co>
2023-07-01 19:05:24 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,185 @@
-import Image from 'next/image'
+import classNames from "classnames";
+import Image from "next/image";
+import Link from "next/link";
+
+const Stripes = () => {
+  return (
+    <div className="absolute left-[50%] right-[50%] top-20">
+      <div className="relative w-full">
+        <div className=" absolute bg-violet-700 rounded-full w-64 h-64 top-[50px] right-[50px] " />
+        <div className="absolute bg-violet-600 rounded-full w-32 h-56 top-5 right-[10px] " />
+        <div className="absolute bg-violet-500 rounded-full w-32 h-48 top-[40px] right-[-30px] " />
+        <div className="absolute bg-violet-400 rounded-full w-32 h-40 top-16 left-[20px] " />
+        <div className="absolute bg-violet-300 rounded-full w-32 h-32 top-20 left-[-20px]" />
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-between">
+      <div className="w-full max-w-screen-lg relative h-[450px] flex items-end justify-end">
+        {/* <Stripes /> */}
+
+        <div className="rotate-90 absolute top-32 -left-10">
+          <div className="text-6xl font-semibold">Lars</div>
+          <div className="text-8xl font-bold font-sans">Hampe</div>
+          <div className="font-mono">Typescript Fullstack Developer</div>
+        </div>
+
+        <Image
+          src="/hashdot.png"
+          alt="Picture of Lars"
+          width={400}
+          height={400}
+        />
+
+        <Image src="/logo.svg" width={100} height={100} alt="HashDot Logo" />
+      </div>
+
+      <div className="bg-neutral-900 grid grid-cols-2 md:grid-cols-4 rounded-lg py-5 px-10 text-white w-full gap-5 max-w-screen-lg">
+        <div className="flex flex-col">
+          <span className="font-bold">.mail</span>
+          <a href="mailto:hello@hashdot.co">hello@hashdot.co</a>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="font-bold">.phone</span>{" "}
+          <a href="tel:+4955120019224">+49 (0)551 200 19 224</a>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="font-bold">.meet</span>{" "}
+          <a href="https://cal.com/hashdot">cal.com/hashdot</a>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="font-bold">.github</span>{" "}
+          <a href="https://github.com/hashdot">github.com/hashdot</a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col gap-10 items-center">
+        <Services />
+        <CTA />
+        <Customers />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <footer className="w-full flex items-center justify-center mt-10">
+        <div className="w-full max-w-screen-lg flex justify-between">
+          <div>&copy; {new Date().getFullYear()} Lars Hampe</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div>
+            <Link href="/impressum">Imprint</Link>
+          </div>
+        </div>
+      </footer>
     </main>
-  )
+  );
 }
+
+const CTA = () => {
+  return (
+    <div className="bg-neutral-900 w-full rounded-lg flex items-center justify-center">
+      <div className="w-full max-w-screen-lg p-10 flex flex-col gap-5">
+        <h3 className="text-6xl text-primary font-sans">
+          Kickoff your next project!
+        </h3>
+        <p className="text-neutral-200 text-xl">
+          Looking for professional assistance in frontend, backend, and DevOps?
+          I offer tailored solutions for your web development and infrastructure
+          needs. Let&apos;s work together on your project and drive it to
+          success. Contact me now to learn more and get started!
+        </p>
+        <a
+          className="bg-primary text-neutral-900 hover:bg-neutral-700 hover:text-white text-xl transition p-5 rounded-lg"
+          href="mailto:hello@hashdot.co?subject=Kickoff my project!&body=Lets do it!"
+        >
+          Let&apos;s do it!
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const Customers = () => {
+  const list = [
+    { name: "Hornbach", logo: "/customers/hornbach.svg" },
+    { name: "Peek & Cloppenburg", logo: "/customers/peek_cloppenburg.svg" },
+    { name: "Convex Energy", logo: "/customers/convex_energy.svg" },
+    { name: "esentri", logo: "/customers/esentri.webp" },
+  ];
+  return (
+    <div className="flex flex-col items-start justify-start w-full max-w-screen-lg gap-5">
+      <h1 className="text-4xl uppercase font-light">Customers</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5 w-full">
+        {list.map((customer) => {
+          return (
+            <div
+              key={customer.name}
+              className="hover:bg-neutral-300 transition rounded-lg h-40 bg-neutral-200 flex flex-col items-center justify-center"
+            >
+              <Image
+                src={customer.logo}
+                width={150}
+                height={50}
+                alt={customer.name}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Services = () => {
+  const list = [
+    {
+      title: "Frontend",
+      text: `Benefit from my comprehensive expertise in Frontend Development! With React and Tailwind CSS as powerful tools, I create tailored, modern, and engaging web applications that showcase your brand effectively. From conceptualization to design and implementation, I prioritize a seamless user experience and optimal performance. Entrust me with the implementation of your web application and enjoy a first-class result that will captivate your target audience. `,
+      style: "bg-sky-500 text-sky-100",
+    },
+    {
+      title: "Backend",
+      text: `Harness the power of Node.js, NestJS, and TypeScript with my top-notch Backend Development services! I specialize in building robust and scalable web applications. By leveraging Node.js, I create high-performance backend systems that handle heavy workloads with ease. NestJS, a progressive Node.js framework, offers a structured and modular approach to development, ensuring clean and maintainable code. With the type safety and enhanced productivity of TypeScript, I deliver efficient and error-free solutions.`,
+      style: "bg-purple-500 text-purple-100",
+    },
+    {
+      title: "DevOps",
+      text: `Unlock the potential of DevOps with my expertise in Docker, Kubernetes, and CI/CD! I offer comprehensive DevOps solutions to streamline your development and deployment processes. By utilizing Docker, I enable efficient containerization of your applications, ensuring portability and scalability. Kubernetes orchestrates these containers, providing automated deployment, scaling, and management. With robust CI/CD pipelines, I automate the build, testing, and deployment phases, reducing errors and accelerating time-to-market. `,
+      style: "bg-green-500 text-green-100",
+    },
+  ];
+  return (
+    <div className="w-full max-w-screen-lg relative gap-5 flex flex-col mt-10">
+      <h1 className="text-4xl uppercase font-light">What can I do for you?</h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        {list.map((service, index) => {
+          return (
+            <div
+              className={classNames(
+                "p-5 flex flex-col gap-3 "
+                // service.style
+              )}
+              key={service.title}
+            >
+              <h2 className="text-4xl text-neutral-400 ">{service.title}</h2>
+              <p
+                className={classNames(
+                  "text-white text-lg rounded-lg p-5 ",
+                  service.style
+                )}
+              >
+                {service.text}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#94E8B4',
+        secondary: '#73AF8A'
       },
+     fontFamily: {
+      sans: ['var(--font-recursive)'],
+      mono: ['var(--font-roboto-mono)']
+     } 
     },
   },
   plugins: [],
